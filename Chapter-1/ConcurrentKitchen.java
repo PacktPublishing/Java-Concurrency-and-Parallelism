d27332a61072311ebd05f9f67a197cc0a1b5d950
jayw04 <37512854+jayw04@users.noreply.github.com>
2023-12-14 20:41:33 -0600

    Update ConcurrentKitchen.java

--- Chapter-1/ConcurrentKitchen.java
+++ Chapter-1/ConcurrentKitchen.java
@@ -1,1 +1,48 @@
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ConcurrentKitchen {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Future<?> task1 = executor.submit(() -> {
+            System.out.println("Chopping vegetables...");
+            // Simulate task
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        try {
+            task1.get(); // Add this line to ensure the task is executed
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            // Handle the exception appropriately
+        }
+
+        Future<?> task2 = executor.submit(() -> {
+            System.out.println("Grilling meat...");
+            // Simulate task
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        try {
+            task2.get(); // Add this line to ensure the task is executed
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            // Handle the exception appropriately
+        }
+
+        executor.shutdown();
+    }
+}
+
